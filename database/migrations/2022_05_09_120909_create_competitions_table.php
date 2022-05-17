904f80ad197d92bcd8c0126002707f6d4e862a4d
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-05-17 08:22:32 +0100

    Ajout role dans table users

--- database/migrations/2022_05_09_120909_create_competitions_table.php
+++ database/migrations/2022_05_09_120909_create_competitions_table.php
@@ -18,14 +18,14 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('club_id')->unsigned();
             $table->bigInteger('discipline_id')->unsigned();
-            $table->string('name_competition');
+            $table->string('name_competition',100);
             $table->date('date_competition');
-            $table->string('address_competition');
-            $table->string('postal_code_competition');
-            $table->string('city_competition');
+            $table->string('address_competition',100);
+            $table->string('postal_code_competition',5);
+            $table->string('city_competition',100);
             $table->double('lat_competition', 8, 6);
             $table->double('lon_competition', 8, 6);
-            $table->string('organizational_details')->nullable();
+            $table->string('organizational_details',200)->nullable();
             $table->timestamps();
             $table->foreign('club_id')
                 ->references('id')
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 });
 
 require __DIR__.'/auth.php';
+
+
